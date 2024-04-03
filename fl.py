4b07981965c8f64ac889ae7317ec67ed440c7268
shxrxx-06 <rameshranusha4@gmail.com>
2024-04-03 22:12:23 +0530

    databse1

--- fl.py
+++ fl.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
 import re
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key='6ede3c79f791dee2f4694d98ee678430'
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='@Shara#23@'
+app.config['MYSQL_DB']='rna'
+mysql=MySQL(app)
 # Function to validate the email
 def is_valid_email(email):
     # Regular expression for validating an Email
@@ -11,6 +18,7 @@ def is_valid_email(email):
         return True
     else:
         return False
+    
 @app.route('/', methods=['GET', 'POST'])
 def home():
     error = None
@@ -30,12 +38,88 @@ def home():
         else:
             # Proceed with account creation
             return redirect(url_for('account'))
+    if request.method=='POST':
+        userDetails=request.form
+        f_name=userDetails['first name']
+        l_name=userDetails['last name']
+        email=userDetails['email']
+        password=userDetails['password']
+        cur=mysql.connection.cursor()
+        cur.execute("INSERT INTO signup(first_name , last_name , email , password) VALUES(%s,%s,%s,%s)",(f_name,l_name,email,password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/login')
+        
 
     return render_template('home.html', error=error)
 
-@app.route('/account')
+@app.route('/database', methods=["GET", "POST"])
+def database_page():
+    if request.method == "POST":
+        ...
+    elif request.method == "GET":
+        return render_template('database.html')
+
+@app.route('/new', methods=["GET", "POST"])
+def new_page():
+    if request.method == "POST":
+        ...
+    elif request.method == "GET":
+        return render_template('new.html')
+@app.route('/en', methods=["GET", "POST"])  
+def en_page():
+    if request.method == "POST":
+        ...
+    elif request.method == "GET":
+        return render_template('encrypt.html')
+@app.route('/va', methods=["GET", "POST"])  
+def va_page():
+    if request.method == "POST":
+        ...
+    elif request.method == "GET":
+        return render_template('va.html')
+@app.route('/login' , methods=["GET","POST"])
+def login():
+    if request.method=='POST':
+        userDetails=request.form
+        email=userDetails['email']
+        password=userDetails['password']
+        cur=mysql.connection.cursor()
+        cur.execute("SELECT * FROM signup WHERE email=%s AND password=%s", (email,password))
+        user=cur.fetchall()
+        if user:
+         session['email']= email 
+         return redirect('/database')
+        else:
+           return "Invalid email or password"
+    return render_template('login.html')
+   
+
+
+
+
+@app.route('/account', methods=["GET","POST"])
 def account():
+    if request.method=='POST':
+        userDetails=request.form
+        f_name=userDetails['first name']
+        l_name=userDetails['last name']
+        email=userDetails['email']
+        password=userDetails['password']
+        cur=mysql.connection.cursor()
+        cur.execute("INSERT INTO signup(first_name , last_name , email , password ) VALUES(%s,%s,%s,%s)",(f_name,l_name,email,password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/login')
     return render_template('account.html')
+
+@app.route('/database')
+def db():
+    if 'email' in session :
+        return render_template('database.html')
+    else :
+        return redirect('/login')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
